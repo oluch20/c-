b74025c27ef0345ac8ea1fb90826c297a14e531a
oluch <olekmlot321@gmail.com>
2026-04-02 07:46:12 +0200

    Refactor code structure for improved readability and maintainability

--- test.cpp
+++ test.cpp
@@ -1,64 +1,116 @@
 #include <iostream>
 
+class MyInt
+{
+    float a;
+    float b;
+    char op;
+    int state = 0; // 0 - correct, 1 - error (/ 0), 2 - error (incorrect operator)
+    float result;
+public:
+    void setA(float x) { a = x; }
+    void setB(float x) { b = x; }
+    void setOp(char x) { op = x; }
+    void SetState(int x) { state = x; }
+
+
+    char getOp() { return (op); }
+    int getState() {return (state);}
+    float getResult() { return (result); }
+
+    void Add()
+    {
+        result = (a + b);
+    }
+
+    void Mul()
+{
+    result = (a * b);
+}
+void Sub()
+{
+    result = (a - b);
+}
+void Div()
+{
+    if (b != 0)
+    {
+        result = (a / b);
+    }
+    else
+    {
+        state = 1;
+    }
+}
+void Pow()
+{
+    float c = a;
+    for (float i = 1; i < b; i++)
+    {
+        a = a * c;
+    }
+    result = (a);
+}
+};
+
 using namespace std;
 
 int main()
 {
-    int state = 0; // 0 - correct, 1 - error (/ 0), 2 - error (incorrect operator)
-    float a;
-    float b;
+    float z;
     char op;
+    MyInt f;
 
     cout << "to jest kalkulator, podaj a" << endl;
-    cin >> a;
+    cin >> z;
+    f.setA(z);
 
     cout << "to jest kalkulator, podaj operator" << endl;
     cin >> op;
+    f.setOp(op);
 
     cout << "to jest kalkulator, podaj b" << endl;
-    cin >> b;
+    cin >> z;
+    f.setB(z);
 
-    switch (op)
+    switch (f.getOp())
     {
     case '+':
     {
-        a = a + b;
-        break;
+        f.Add();
+         break;
     }
     case '-':
     {
-        a = a - b;
+        f.Sub();
         break;
     }
     case '*':
     {
-        a = a + b;
+        f.Mul();
         break;
     }
     case '/':
     {
-        if (b != 0)
-        {
-            a = a / b;
-        }
-        else
-        {
-            state = 1;
-        }
+        f.Div();
+        break;
     }
-
-    break;
-
-    default:
-        state = 2;
+    case '^':
+    {
+        f.Pow();
         break;
     }
 
-    switch (state)
+    default:
+        f.SetState(2);
+        break;
+    }
+
+    switch (f.getState())
     {
     case 0:
     {
-        cout << "Wynik to: " << a << endl;
+        cout << "Wynik to: " << f.getResult() << endl;
         break;
     }
     break;
